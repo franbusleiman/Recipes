7077227c9f63c0719e662c44b85c68361a758b1e
franbusleiman <72284104+franbusleiman@users.noreply.github.com>
2020-12-05 19:54:52 -0300

    Closes #13

---
+++ src/main/java/Franciscobusleiman/recipes/recipes/controllers/RecipeController.java
@@ -0,0 +1,27 @@
+package Franciscobusleiman.recipes.recipes.controllers;
+
+import Franciscobusleiman.recipes.recipes.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("show/{id}")
+    public String getRecipe(@PathVariable String id, Model model){
+
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+
+        return "show.html";
+    }
+}
